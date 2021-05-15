f74d1bce2be881886bc24c605d14f0678f310484
Portatil <Portatil@Portatil-PC>
2021-05-15 10:10:58 +0200

    cambios en import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-/* Creamos los usuarios*/ 
+/* Creamos los usuarios*/  
 INSERT INTO `usuarios` (nombreusuario, clave, activo) VALUES ('gestor','$2a$10$O9wxmH/AeyZZzIS09Wp8YOEMvFnbRVJ8B4dmAMVSGloR62lj.yqXG',1);
 INSERT INTO `usuarios` (nombreusuario, clave, activo) VALUES ('region1','$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS',1);
 INSERT INTO `usuarios` (nombreusuario, clave, activo) VALUES ('centro1','$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS',1);
@@ -180,7 +180,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 1, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 1, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 1, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 1, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 1, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 1, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 1, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 1, 8);
 
@@ -189,7 +189,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('40-49', 2, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 2, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 2, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Antígenos', 2, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('20', 2, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('200', 2, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Manolo', 2, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 2, 8);
 
@@ -198,7 +198,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 3, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 3, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 3, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 3, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 3, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 3, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 3, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 3, 8);
 
@@ -207,7 +207,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('40-49', 4, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 4, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 4, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Antígenos', 4, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('20', 4, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('200', 4, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Manolo', 4, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 4, 8);
 
@@ -216,7 +216,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 5, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 5, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 5, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 5, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 5, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 5, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 5, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 5, 8);
 
@@ -225,7 +225,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('40-49', 6, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 6, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 6, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Antígenos', 6, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('20', 6, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('200', 6, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Manolo', 6, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 6, 8);
 
@@ -234,7 +234,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 7, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 7, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 7, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 7, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 7, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 7, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 7, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 7, 8);
 
@@ -243,7 +243,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 8, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 8, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 8, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 8, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 8, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 8, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 8, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 8, 8);
 
@@ -252,7 +252,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 9, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 9, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 9, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 9, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 9, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 9, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 9, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 9, 8);
 
@@ -261,7 +261,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 10, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 10, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 10, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 10, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 10, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 10, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 10, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 10, 8);
 
@@ -270,7 +270,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 11, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 11, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 11, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 11, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 11, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 11, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 11, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 11, 8);
 
@@ -279,7 +279,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 12, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 12, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 12, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 12, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 12, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 12, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 12, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 12, 8);
 
@@ -288,7 +288,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 13, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 13, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 13, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 13, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 13, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 13, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 13, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 13, 8);
 
@@ -297,7 +297,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 14, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 14, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 14, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 14, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 14, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 14, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 14, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 14, 8);
 
@@ -306,7 +306,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 15, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 15, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 15, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 15, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 15, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 15, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 15, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 15, 8);
 
@@ -315,7 +315,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 16, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 16, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 16, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 16, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 16, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 16, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 16, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 16, 8);
 
@@ -324,7 +324,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 17, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 17, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 17, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 17, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 17, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 17, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 17, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 17, 8);
 
@@ -333,7 +333,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 18, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 18, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 18, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 18, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 18, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 18, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 18, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 18, 8);
 
@@ -342,7 +342,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 19, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 19, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 19, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 19, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 19, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 19, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 19, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 19, 8);
 
@@ -351,7 +351,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 20, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 20, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 20, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 20, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 20, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 20, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 20, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 20, 8);
 
@@ -360,7 +360,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 21, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 21, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 21, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 21, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 21, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 21, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 21, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 21, 8);
 
@@ -369,7 +369,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 22, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 22, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 22, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 22, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 22, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 22, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 22, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 22, 8);
 
@@ -378,7 +378,7 @@ INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('30-39', 23, 2);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Si', 23, 3);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('No', 23, 4);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Vírica', 23, 5);
-INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('10', 23, 6);
+INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('100', 23, 6);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('Felipe', 23, 7);
 INSERT INTO datos (dato, datosperfil_id, pregunta_id) VALUES('2017-08-19', 23, 8);
 
